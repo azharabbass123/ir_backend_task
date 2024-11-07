9505083c360f79bb409498ed34dba9dfa0913e47
Azhar Abbass <azharabbass786.com@gmail.com>
2024-11-07 14:51:56 +0500

    Created required modules, controllers and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TasksModule } from './tasks/tasks.module';
+import { ProjectsModule } from './projects/projects.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { AnalyticsModule } from './analytics/analytics.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+    UsersModule, 
+    TasksModule, 
+    ProjectsModule, AnalyticsModule, AuthModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import  * as mongoose from 'mongoose';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const db = mongoose.connection;
+  db.on('connected', () => {
+    console.log('Connected to the database successfully');
+  });
+  db.on('error', (error) => {
+    console.error('Error connecting to the database:', error);
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
